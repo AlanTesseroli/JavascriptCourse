a3bcf77023e91f5d7a04164e2de24848a2a2e30f
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-03-15 08:56:39 -0300

    15/03

--- Aula 13 - Exercicios/Parte 2/javascript.js
+++ Aula 13 - Exercicios/Parte 2/javascript.js
@@ -3,6 +3,9 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+    
+    
+    
     if (fano.value.length == 0 || fano.value > 'txtano'){
         window.alert('ERRO, tente novamente!')
     }
