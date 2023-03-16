c1138c6d74da09677639cffc59617f6c64cacbac
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-03-16 08:58:33 -0300

    16/03 - Final

--- Aula 13 - Exercicios/Parte 2/javascript.js
+++ Aula 13 - Exercicios/Parte 2/javascript.js
@@ -14,6 +14,59 @@ function verificar(){
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createElement('img')
-        res.innerHTML = `Idade calculada: ${idade}`
-    }
+
+        if (fsex[0].checked){
+            //Masculino
+            genero = 'Homem'
+                if (idade < 10){
+                    //Criança
+                    img.setAttribute('src', 'homcri.jpg')
+                }
+                else if( idade <21){
+                    //Jovem
+                    img.setAttribute('src', 'homjov.jpg')
+                }
+
+                else if(idade <50){
+                    //Adulto
+                    img.setAttribute('src', 'hommed.jpg')
+                }
+
+                else{
+                    //Idoso
+                    img.setAttribute('src', 'homido.jpg')
+                }
+
+        }
+
+        else if(fsex[1].checked){
+        //Feminino
+           genero = 'Mulher'
+            if (idade < 10){
+                    //Criança
+                    img.setAttribute('src', 'mulcri.jpg')
+                }
+                else if( idade <21){
+                    //Jovem
+                    img.setAttribute('src', 'muljov.jpg')
+                }
+
+                else if(idade <50){
+                    //Adulto
+                    img.setAttribute('src', 'mulmei.jpg')
+                }
+
+                else{
+                    //Idoso
+                    img.setAttribute('src', 'mulido.jpg')
+                }
+
+            
+            }
+
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos de idade`
+        res.appendChild(img)
+
 }
