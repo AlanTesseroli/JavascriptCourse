463a531edafe290cb813212547480b21d0704166
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-03-20 19:04:58 -0300

    Exercicio parte 2

--- Aula 14/Exercicios/Ex016/Ex017/javascript.js
+++ Aula 14/Exercicios/Ex016/Ex017/javascript.js
@@ -1,18 +1,37 @@
 function tabuada(){
     var ini = document.getElementById('txtini')
-    var res = document.getElementById('res')    
+    var select = document.getElementById('tabu')   
 
     var num = Number(ini.value)
-    var c = 0
-
-    while(c <= 10){
-        res.innerHTML += '<br>'
-        res.innerHTML += `${c} x ${num} = ${c*num}`
-        c++
-        
+    var c = 1
+    
+    if(ini.value.lenght == 0){
+        window.alert('Tente novamente!')
     }
 
+    else{
 
+
+        while(c <= 10){
+            var opcao = document.createElement('option')
+            opcao.text = `${c} x ${num} = ${c*num}`
+            select.appendChild(opcao)
+            //opt.innerHTML += '<br>'
+            //opt.innerHTML += `${c} x ${num} = ${c*num}`
+            c++
+            
+        }
+    }
+
+    /*
+        while(c <= 10){
+                res.innerHTML += '<br>'
+                res.innerHTML += `${c} x ${num} = ${c*num}`
+                c++
+                
+            }
+
+    */
 }
 
 
