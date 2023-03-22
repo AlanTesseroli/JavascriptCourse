f625891661fdddf48ff9d895e59614c808fefd9c
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-03-22 08:55:38 -0300

    Exercicio js 95% pronto

--- Aula 16 - Exercicios/javascript.js
+++ Aula 16 - Exercicios/javascript.js
@@ -1,6 +1,13 @@
 
 const lista = []
 
+
+
+
+
+
+
+
 function tabuada(){
     var ini = document.getElementById('txtini')
     var select = document.getElementById('tabu')   
@@ -9,25 +16,41 @@ function tabuada(){
 
 
     var num = Number(ini.value)
-    var c = 0
 
 
             if(num <= 100 && num > 0){
                 
+                res.innerHTML = 'Preencha os dados para ver o resultado'
                 var opcao = document.createElement('option')
                 opcao.text = `Valor ${num} adicionado`
                 select.appendChild(opcao)
 
                 lista.push(num)
 
-                document.getElementById('fim').onclick = function(){
-                    res.innerHTML += `Ao todo, temos ${lista.length} números` 
-                    res.innerHTML += `O maior valor foi `
-                    res.innerHTML += `O menor valor foi `
-                    res.innerHTML += `Somando os valores temos `
-                    res.innerHTML += `A média dos valores digitados é  `
-                }
 
+                
+                document.getElementById('fim').onclick = function(){
+
+                    const max = Math.max(...lista)
+                    const min = Math.min(...lista)
+
+                    function somatotal(lista){
+                        var soma = 0
+
+                        for(var count = 0; count < lista.length; count += 1){
+                            soma += lista[count]
+
+                        }
+                        return soma
+                    }
+
+                    res.innerHTML = ''
+                    res.innerHTML += `Ao todo, temos ${lista.length} números`
+                    res.innerHTML += `O maior valor foi ${max}`
+                    res.innerHTML += `O menor valor foi ${min}`
+                    res.innerHTML += `Somando os valores temos ${somatotal(lista)}`
+                    res.innerHTML += `A média dos valores digitados é  ${somatotal(lista)/lista.length}`
+                }
 
 
             }
@@ -39,6 +62,18 @@ function tabuada(){
     
 
 }
+
+document.getElementById('fim').onclick = function(){ 
+    if(lista.length==0){
+        window.alert('Tente novamente')
+    }
+    
+    else{
+
+    }
+
+}
+
 
 
 function reiniciar(){
